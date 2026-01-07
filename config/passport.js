565114ffcc992e01d8bea1fc36c6d3bd251a67ef
yendexgames-create <yendexgames@gmail.com>
2026-01-07 21:41:10 +0500

    sdsdsadasdasdasd

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || 'GOOGLE_CLIENT_ID_HERE',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'GOOGLE_CLIENT_SECRET_HERE',
-      callbackURL: '/auth/google/callback'
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
